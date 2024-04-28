6cfa02b24d76ff8ba4d62a14dcb8016c6049294c
AngryWolf <qrer71@yandex.ru>
2024-04-28 23:28:52 +0300

    Create a function to check an object for coroutine

---
+++ src/speedy/utils/predicates.py
@@ -0,0 +1,18 @@
+import asyncio
+from typing import TypeVar, ParamSpec, Callable, TypeGuard, Awaitable
+
+from .helpers import unwrap_partial
+
+P = ParamSpec('P')
+T = TypeVar('T')
+
+
+def is_async_callable(
+        obj: Callable[P, T]
+) -> TypeGuard[Callable[P, Awaitable[T]]]:
+    obj = unwrap_partial(obj)
+
+    return asyncio.iscoroutinefunction(obj) or (
+            callable(obj) and
+            asyncio.iscoroutinefunction(obj.__call__)  # type: ignore
+    )
