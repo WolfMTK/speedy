0e0739313b891318ad89a36dfe3cf9c500267ff0
AngryWolf <qrer71@yandex.ru>
2024-04-28 23:30:21 +0300

    Rename files

--- src/speedy/types/application.py
+++
@@ -1,7 +0,0 @@
-from typing import Callable, Awaitable
-
-from .asgi_types import Scope, ASGIReceiveCallable, ASGISendCallable
-
-type ASGIApplication = Callable[
-    [Scope, ASGIReceiveCallable, ASGISendCallable], Awaitable[None]
-]
