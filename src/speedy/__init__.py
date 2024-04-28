d3df4809bf5cb987ba03a721684096e8b7cff739
AngryWolf <qrer71@yandex.ru>
2024-04-28 19:23:46 +0300

    Update imports

--- src/speedy/__init__.py
+++ src/speedy/__init__.py
@@ -1,10 +1,7 @@
 from speedy import status_code as status
 from speedy.application import Speedy
-from speedy.enums import HttpMethod, ScopeType
 
 __all__ = (
     'status',
     'Speedy',
-    'HttpMethod',
-    'ScopeType'
 )
