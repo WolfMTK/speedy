6da257455a6eac3368d70e70fc000c25c1fe58ff
AngryWolf <qrer71@yandex.ru>
2024-04-28 23:24:10 +0300

    Update imports and replace by an abstract class

--- src/speedy/protocols/application.py
+++ src/speedy/protocols/application.py
@@ -1,9 +1,9 @@
-from typing import Protocol
+from abc import ABC
 
 from speedy.types import Scope, ASGIReceiveCallable, ASGISendCallable
 
 
-class BaseASGIApplication(Protocol):
+class BaseASGIApplication(ABC):
     """ Base ASGI application protocol. """
 
     async def __call__(
