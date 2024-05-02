dfa63b49bf08bca496283ccb39e1efa3375ff272
AngryWolf <qrer71@yandex.ru>
2024-05-02 21:17:55 +0300

    Create docstring and update signature of methods

--- src/speedy/protocols/endpoints.py
+++ src/speedy/protocols/endpoints.py
@@ -17,19 +17,23 @@ class BaseEndpoint(ABC):
 
 
 class BaseHTTPEndpoint(BaseEndpoint):
+    """ Abstract class of the base http endpoints. """
+
     @abstractmethod
     async def method_not_allowed(self, request: Request): ...
 
 
 class BaseWebSocketEndpoint(BaseEndpoint):
+    """ Abstract class of the base websocket endpoints. """
+
     @abstractmethod
     async def decode(self, websocket: WebSocket, message: Message) -> Any: ...
 
     @abstractmethod
-    async def on_connect(self) -> None: ...
+    async def on_connect(self, websocket: WebSocket) -> None: ...
 
     @abstractmethod
-    async def on_receive(self) -> None: ...
+    async def on_receive(self, websocket: WebSocket, data: Any) -> None: ...
 
     @abstractmethod
-    async def on_disconnect(self) -> None: ...
+    async def on_disconnect(self, websocket: WebSocket, code: int) -> None: ...
