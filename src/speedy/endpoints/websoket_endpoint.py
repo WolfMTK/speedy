8eb73a2e3502c9bd8be65e8fab127ba43ae6a4f9
AngryWolf <qrer71@yandex.ru>
2024-05-02 21:24:44 +0300

    Update docstring and method to decode json

--- src/speedy/endpoints/websoket_endpoint.py
+++ src/speedy/endpoints/websoket_endpoint.py
@@ -10,6 +10,9 @@ from speedy.websocket import WebSocket
 
 
 class WebSocketEndpoint(BaseWebSocketEndpoint):
+    encoding: str | None = None
+    json_library: str = 'json'
+
     def __init__(
             self,
             scope: Scope,
@@ -20,15 +23,6 @@ class WebSocketEndpoint(BaseWebSocketEndpoint):
         self.scope = scope
         self.receive = receive
         self.send = send
-        self._encoding: str | WebSocketEncoding | None = None
-
-    @property
-    def encoding(self) -> str | WebSocketEncoding | None:
-        return self._encoding
-
-    @encoding.setter
-    def encoding(self, value: str | WebSocketEncoding | None) -> None:
-        self._encoding = value
 
     async def dispatch(self) -> None:
         ...
@@ -37,33 +31,63 @@ class WebSocketEndpoint(BaseWebSocketEndpoint):
             self,
             websocket: WebSocket,
             message: Message,
-            json_library: str = 'json'
     ) -> Any:
         """
         Decode a message sent over a websocket.
 
         Supported libraries for decoding to json: `json`, `ujson`, `orjson`.
         """
-        match self._encoding:
+        match self.encoding:
             case WebSocketEncoding.TEXT:
                 return await self._decode_text(websocket, message)
             case WebSocketEncoding.BYTES:
                 return await self._decode_bytes(websocket, message)
             case WebSocketEncoding.JSON:
-                return await self._decode_json(websocket, message, json_library)
+                return await self._decode_json(websocket, message)
 
         if message.get(WebSocketEncoding.TEXT):
             return message[WebSocketEncoding.TEXT]
         return message[WebSocketEncoding.BYTES]
 
-    async def on_connect(self) -> None:
-        ...
+    async def on_connect(self, websocket: WebSocket) -> None:
+        """
+        Override to handle an incoming websocket connection.
 
-    async def on_receive(self) -> None:
-        ...
+        Accepts the connection when the websocket is open:
+        ```
+            await websocket.accept()
+        ```
+        """
 
-    async def on_disconnect(self) -> None:
-        ...
+    async def on_receive(self, websocket: WebSocket, data: Any) -> None:
+        """
+        Override to handle an incoming websocket message.
+
+        Send bytes:
+        ```
+            await websocket.send_bytes(data)
+        ```
+        Send text:
+        ```
+            await websocket.send_text(data)
+        ```
+        Send json:
+        ```
+            await websocket.send_json(data, mode='text', json_library='json')
+        ```
+            mode takes as arguments: 'text' or 'bytes'
+            json_library takes as arguments: 'json', 'ujson', 'orjson'
+        """
+
+    async def on_disconnect(self, websocket: WebSocket, code: int) -> None:
+        """
+        Override to handle a disconnecting websocket.
+
+        Closing the connection:
+        ```
+            await websocket.close(code=code)
+        ```
+        """
 
     async def _decode_text(self, websocket: WebSocket, message: Message) -> Any:
         if WebSocketEncoding.TEXT not in message:
@@ -76,22 +100,23 @@ class WebSocketEndpoint(BaseWebSocketEndpoint):
             await websocket.close(code=WS_1003_UNSUPPORTED_DATA)
             raise RuntimeWebSocketException('Expected bytes websocket messages')
 
-    async def _decode_json(self, websocket: WebSocket, message: Message, json_library: str) -> Any:
+    async def _decode_json(self, websocket: WebSocket, message: Message) -> Any:
+        if self.json_library not in ('json', 'ujson', 'orjson'):
+            self.json_library = 'json'
+
         if (text := message.get(WebSocketEncoding.BYTES)) is not None:
             text = text.decode('utf-8')
         else:
             text = message[WebSocketEncoding.TEXT]
 
-        if json_library == 'ujson':
+        if self.json_library == 'ujson':
             import ujson
-
             try:
                 return ujson.loads(text)  # type: ignore
             except ujson.JSONDecodeError:
                 exception_flag = True
-        elif json_library == 'orjson':
+        elif self.json_library == 'orjson':
             import orjson
-
             try:
                 return orjson.loads(text)  # type: ignore
             except orjson.JSONDecodeError:
