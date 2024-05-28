a5368ae427820fb26dce03a326dfcc4a1ccde0c2
AngryWolf <qrer71@yandex.ru>
2024-05-28 14:09:31 +0300

    Create enums

---
+++ src/speedy/enums.py
@@ -0,0 +1,18 @@
+from enum import StrEnum
+
+
+class HttpMethod(StrEnum):
+    GET = 'GET'
+    POST = 'POST'
+    DELETE = 'DELETE'
+    PATCH = 'PATCH'
+    PUT = 'PUT'
+    HEAD = 'HEAD'
+    TRACE = 'TRACE'
+    OPTIONS = 'OPTIONS'
+
+
+class ScopeType(StrEnum):
+    HTTP = 'http'
+    WEBSOCKET = 'websocket'
+    LIFESPAN = 'lifespan'
