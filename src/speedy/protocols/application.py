a7e0684bb8c950c9a1220e43760a93284541c547
AngryWolf <qrer71@yandex.ru>
2024-05-28 15:32:14 +0300

    Create method build_middleware_stack

--- src/speedy/protocols/application.py
+++ src/speedy/protocols/application.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
 
 from speedy.types import Scope, ASGIReceiveCallable, ASGISendCallable
+from speedy.types.application import ASGIAppType
 
 
 class ASGIApplication(ABC):
     @abstractmethod
     async def __call__(self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None: ...
+
+    @abstractmethod
+    def build_middleware_stack(self) -> ASGIAppType: ...
