d00398d0bbcbd253658cdb27d867584cb57bfa3d
AngryWolf <qrer71@yandex.ru>
2024-05-28 14:52:16 +0300

    Create protocols that implement middleware

---
+++ src/speedy/protocols/middleware.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from typing import Protocol, runtime_checkable, ParamSpec
+
+from speedy.types import ASGISendCallable, ASGIReceiveCallable, Scope
+
+P = ParamSpec('P')
+
+
+@runtime_checkable
+class MiddlewareProtocol(Protocol[P]):
+    async def __call__(self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None: ...
+
+
+class BaseMiddleware(ABC):
+    @abstractmethod
+    async def __call__(self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None: ...
