5fffd98ba521c389f98bfffaa17739e8a55ca782
AngryWolf <qrer71@yandex.ru>
2024-05-28 16:32:31 +0300

    Create a method to get the user's middleware

--- src/speedy/application.py
+++ src/speedy/application.py
@@ -1,11 +1,15 @@
+from typing import Sequence, Any
+
+from speedy.middleware import Middleware
 from speedy.protocols.application import ASGIApplication
 from speedy.types import Scope, ASGIReceiveCallable, ASGISendCallable
 from speedy.types.application import ASGIAppType
 
 
 class Speedy(ASGIApplication):
-    def __init__(self) -> None:
+    def __init__(self, middleware: Sequence[Middleware] | None = None) -> None:
         self.middleware_stack: ASGIAppType | None = None
+        self.user_middleware = self._get_user_middleware(middleware)
 
     async def __call__(self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None:
         scope['app'] = self
@@ -17,3 +21,8 @@ class Speedy(ASGIApplication):
 
     def build_middleware_stack(self) -> ASGIAppType:
         return self
+
+    def _get_user_middleware(self, middleware: Sequence[Middleware] | None) -> list[Middleware]:
+        if middleware is None:
+            return []
+        return list(middleware)
