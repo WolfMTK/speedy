7a2d42ef21b59c7246a875c869059836f05dbf5d
AngryWolf <qrer71@yandex.ru>
2024-05-30 19:42:46 +0300

    Update tests

--- tests/test_datastructures/test_url.py
+++ tests/test_datastructures/test_url.py
@@ -1,4 +1,4 @@
-from speedy.datastructures import URL
+from speedy.datastructures.url import URL
 
 
 def test_url() -> None:
@@ -14,46 +14,22 @@ def test_url() -> None:
     assert url.fragment == "anchor"
 
 
-def test_url_replace() -> None:
-    url = URL('https://example.org:8000/path/to/somewhere?abc=123#anchor')
-    new_url = url.replace(scheme='http')
-    assert new_url == 'http://example.org:8000/path/to/somewhere?abc=123#anchor'
-    assert new_url.scheme == 'http'
-
-    new_url = url.replace(port=None)
-    assert new_url == 'https://example.org/path/to/somewhere?abc=123#anchor'
-    assert new_url.port is None
-
-    new_url = url.replace(hostname='example.com')
-    assert new_url == 'https://example.com:8000/path/to/somewhere?abc=123#anchor'
-    assert new_url.hostname == 'example.com'
-
-
-def test_ipb6_url_replace() -> None:
-    ipv6_url = URL('https://[fe::2]:12345')
-    new_url = ipv6_url.replace(port=8000)
-    assert new_url == 'https://[fe::2]:8000'
-
-    new_url = ipv6_url.replace(username='username', password='password')
-    assert new_url == 'https://username:password@[fe::2]:12345'
-    assert new_url.netloc == 'username:password@[fe::2]:12345'
-
-    ipv6_url = URL('https://[fe::2]')
-    new_url = ipv6_url.replace(port=8000)
-    assert new_url == 'https://[fe::2]:8000'
-
-
 def test_url_eq() -> None:
-    url = URL('http://u:p@host/')
-    assert url.replace(hostname='bar') == URL('http://u:p@bar/')
-
-    url = URL('http://u:p@host:80')
-    assert url.replace(port=88) == URL('http://u:p@host:88')
-
-    url = URL('http://host:80')
-    assert url.replace(username='u') == URL('http://u@host:80')
+    assert URL('') == URL('')
+    assert URL('/foo') == '/foo'
+    assert URL('') != 1
 
 
 def test_url_repr() -> None:
     url = URL('https://example.org:8000/path/to/somewhere?abc=123#anchor')
     assert repr(url) == "URL('https://example.org:8000/path/to/somewhere?abc=123#anchor')"
+
+
+def test_url_replace_query() -> None:
+    url = URL('https://example.org:8000/path/to/somewhere?abc=123#anchor')
+    assert url.query == "abc=123"
+    url = url.replace_query(order='name')
+    assert str(url) == 'https://example.org:8000/path/to/somewhere?order=name#anchor'
+    assert url.query == 'order=name'
+    url = URL('https://example.org/path/to?a=1&b=2')
+    assert url.query == 'a=1&b=2'
