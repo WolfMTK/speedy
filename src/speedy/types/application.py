4caf2c27b31ba8235fded1c67d6895edb9aff9e5
AngryWolf <qrer71@yandex.ru>
2024-06-01 15:05:34 +0300

    Update signature

--- src/speedy/types/application.py
+++ src/speedy/types/application.py
@@ -1,5 +1,15 @@
 from typing import Callable, Awaitable
 
-from .asgi_types import Scope, ASGIReceiveCallable, ASGISendCallable
+from .asgi_types import (
+    Scope,
+    LifespanScope,
+    ASGIReceiveCallable,
+    ASGISendCallable,
+    LifeSpanReceiveMessage,
+    LifeSpanSendMessage,
+)
 
-ASGIAppType = Callable[[Scope, ASGIReceiveCallable, ASGISendCallable], Awaitable[None]]
+ASGIAppType = Callable[[Scope | LifespanScope,
+                        ASGIReceiveCallable | LifeSpanReceiveMessage,
+                        ASGISendCallable |
+                        LifeSpanSendMessage], Awaitable[None]]
