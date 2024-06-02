8690721283217e76dcf84853e5bed673a6058996
AngryWolf <qrer71@yandex.ru>
2024-06-02 16:38:06 +0300

    Update signature

--- src/speedy/protocols/websockets.py
+++ src/speedy/protocols/websockets.py
@@ -1,7 +1,19 @@
 from abc import ABC
+from typing import TypeVar, Iterable
 
-from speedy.types import ASGIReceiveCallable
+from speedy.datastructures import Headers
+from speedy.types import ASGIReceiveCallable, ASGISendCallable
 
 
 class AbstractWebsocket(ABC):
     def receive_wrapper(self, receive: ASGIReceiveCallable) -> ASGIReceiveCallable: ...
+
+    def send_wrapper(self, send: ASGISendCallable) -> ASGIReceiveCallable: ...
+
+    def close(self, code: int, reason: str | None) -> None: ...
+
+    def accept(
+            self,
+            subprotocols: str | None = None,
+            headers: Headers | Iterable[tuple[bytes, bytes]] | None = None
+    ): ...
