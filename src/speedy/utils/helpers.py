7da055728066fdd8e458c8c7119e51a8deb9d243
AngryWolf <qrer71@yandex.ru>
2024-06-09 19:32:36 +0300

    Fix imports

--- src/speedy/utils/helpers.py
+++ src/speedy/utils/helpers.py
@@ -1,13 +1,15 @@
 import functools
 from typing import TypeVar, cast
 
-from .sync import AsyncCallable
-
 T = TypeVar('T')
 
 
 def unwrap_partial(value: T) -> T:
     """ Unwraps a partial, returning the underlying callable. """
+
+    # INFO: Bypassing cyclical imports
+    from .sync import AsyncCallable
+
     return cast(
         'T', value.func if isinstance(
             value, (functools.partial, AsyncCallable)
