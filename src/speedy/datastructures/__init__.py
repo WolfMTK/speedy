fca4db0f46ea2f37d893c8fc47ae24ba3c017701
AngryWolf <qrer71@yandex.ru>
2024-06-10 12:38:34 +0300

    Update imports

--- src/speedy/datastructures/__init__.py
+++ src/speedy/datastructures/__init__.py
@@ -1,11 +1,12 @@
 from .url import URL
-from .headers import Headers
+from .headers import Headers, MutableHeaders
 from .upload_file import UploadFile
 from .state import State
 
 __all__ = (
     'URL',
     'Headers',
+    'MutableHeaders',
     'UploadFile',
     'State'
 )
