24b204f79809832371e2f41d0abe83fa220f03d3
Alexander <qrer71@yandex.ru>
2024-07-12 13:48:04 +0300

    Update protocols

--- src/speedy/protocols/middleware.py
+++ src/speedy/protocols/middleware.py
@@ -1,16 +1,23 @@
 from abc import ABC, abstractmethod
 from typing import Protocol, runtime_checkable, ParamSpec
 
+from speedy.protocols import ASGIApplication
 from speedy.types import ASGISendCallable, ASGIReceiveCallable, Scope
 
 P = ParamSpec('P')
 
 
 @runtime_checkable
-class MiddlewareProtocol(Protocol):
+class MiddlewareProtocol(Protocol[P]):
+    def __init__(self, app: ASGIApplication, *args: P.args, **kwargs: P.kwargs) -> None: ...
+
     async def __call__(self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None: ...
 
 
-class BaseMiddleware(ABC):
+class AbstractMiddleware(ABC):
     @abstractmethod
     async def __call__(self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None: ...
+
+    # TODO: add arguments and typing
+    @abstractmethod
+    async def dispatch(self): ...
