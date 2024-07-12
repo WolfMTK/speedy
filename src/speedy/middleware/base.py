2dfe90c7cd1d74ffdd00771946597f09b0f272f3
Alexander <qrer71@yandex.ru>
2024-07-12 13:46:10 +0300

    Update signature Middleware

--- src/speedy/middleware/base.py
+++ src/speedy/middleware/base.py
@@ -6,6 +6,7 @@ P = ParamSpec('P')
 
 
 class Middleware:
+    """ Middleware class. """
     def __init__(self, cls: type[MiddlewareProtocol], *args: P.args, **kwargs: P.kwargs) -> None:
         self.cls = cls
         self.args = args
@@ -13,3 +14,20 @@ class Middleware:
 
     def __iter__(self) -> Iterator[Any]:
         return iter((self.cls, self.args, self.kwargs))
+
+    def __repr__(self):
+        _class = type(self).__name__
+        args = ', '.join(
+            [self.cls.__name__]
+            + list(self._get_args_to_string())
+            + list(self._get_option_to_string())
+        )
+        return f'{_class}({args})'
+
+    def _get_args_to_string(self) -> Iterator[str]:
+        for value in self.args:
+            yield f'{value!r}'
+
+    def _get_option_to_string(self) -> Iterator[str]:
+        for key, value in self.kwargs:
+            yield f'{key}={value!r}'
