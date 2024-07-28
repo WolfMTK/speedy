1454a007b7b1ac81ab031d65eb97544abd24274d
Alexander <qrer71@yandex.ru>
2024-07-28 18:37:05 +0300

    Update imports

--- src/speedy/datastructures/__init__.py
+++ src/speedy/datastructures/__init__.py
@@ -2,9 +2,12 @@ from .url import URL, URLPath
 from .headers import Headers, MutableHeaders
 from .upload_file import UploadFile
 from .state import State
+from .multi_dicts import ImmutableMultiDict
+from .multi_dicts import MultiDict
 
 __all__ = (
     'URL',
+    'ImmutableMultiDict',
     'Headers',
     'MutableHeaders',
     'UploadFile',
--- src/speedy/protocols/__init__.py
+++ src/speedy/protocols/__init__.py
@@ -1,8 +1,10 @@
 from .application import ASGIApplication
 from .middleware import MiddlewareProtocol, AbstractMiddleware
+from .multi_dicts import MultiMapping
 
 __all__ = (
     'ASGIApplication',
     'MiddlewareProtocol',
-    'AbstractMiddleware'
+    'AbstractMiddleware',
+    'MultiMapping'
 )
