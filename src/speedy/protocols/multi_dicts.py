200877095eb603161411390a7ac91fb8941d23bf
Alexander <qrer71@yandex.ru>
2024-08-09 17:27:34 +0300

    Update signature

--- src/speedy/protocols/multi_dicts.py
+++ src/speedy/protocols/multi_dicts.py
@@ -9,13 +9,6 @@ class MultiMapping[_Key, _Value](ABC):
     _dict: dict[_Key, _Value]
     _stack: list[tuple[Any, Any]]
 
-    def __init__(
-            self,
-            *args: MultiMapping[_Key, _Value] | Mapping[_Key, _Value] | Iterable[tuple[_Key, _Value]],
-            **kwargs: Any
-    ) -> None:
-        self._check_args(*args)
-
     def __setitem__(self, key: _Key, values: list[_Value]) -> None:
         items = [(k, v) for (k, v) in self._stack if k != key]
         self._stack = items + [(key, value) for value in values]
@@ -51,56 +44,37 @@ class MultiMapping[_Key, _Value](ABC):
             self,
             *args: Mapping[_Key, _Value] | MultiMapping[_Key, _Value] | list[tuple[Any, Any]],
             **kwargs: Any
-    ) -> None:
-        ...
+    ) -> None: ...
 
     @abstractmethod
-    def get(self, key: Any, default: Any):
-        ...
+    def get(self, key: Any, default: Any): ...
 
     @abstractmethod
-    def append(self, key: Any, value: Any) -> None:
-        ...
+    def append(self, key: Any, value: Any) -> None: ...
 
     @abstractmethod
-    def getList(self, key: Any) -> list[_Value]:
-        ...
+    def getList(self, key: Any) -> list[_Value]: ...
 
     @abstractmethod
-    def clear(self) -> None:
-        ...
+    def clear(self) -> None: ...
 
     @abstractmethod
-    def pop(self, key: Any, default: Any):
-        ...
+    def pop(self, key: Any, default: Any): ...
 
     @abstractmethod
-    def popitem(self) -> tuple[Any, Any]:
-        ...
+    def popitem(self) -> tuple[Any, Any]: ...
 
     @abstractmethod
-    def poplist(self, key: Any) -> list[Any]:
-        ...
+    def poplist(self, key: Any) -> list[Any]: ...
 
     @abstractmethod
-    def keys(self) -> KeysView[_Key]:
-        ...
+    def keys(self) -> KeysView[_Key]: ...
 
     @abstractmethod
-    def values(self) -> ValuesView[_Value]:
-        ...
+    def values(self) -> ValuesView[_Value]: ...
 
     @abstractmethod
-    def items(self) -> ItemsView[_Key, _Value]:
-        ...
+    def items(self) -> ItemsView[_Key, _Value]: ...
 
     @abstractmethod
-    def multi_items(self) -> list[tuple[_Key, _Value]]:
-        ...
-
-    def _check_args(
-            self,
-            *args: MultiMapping[_Key, _Value] | Mapping[_Key, _Value] | Iterable[tuple[_Key, _Value]]
-    ) -> None:
-        if not len(args) < 2:
-            raise AttributeError('Too many arguments.')
+    def multi_items(self) -> list[tuple[_Key, _Value]]: ...
