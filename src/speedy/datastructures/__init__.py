5f4b40146ba210d8854e67724f964e0c8bf3a676
Alexander <qrer71@yandex.ru>
2024-08-13 13:33:54 +0300

    Add new import

--- src/speedy/datastructures/__init__.py
+++ src/speedy/datastructures/__init__.py
@@ -3,7 +3,7 @@ from .headers import Headers, MutableHeaders
 from .multi_dicts import ImmutableMultiDict, MultiDict
 from .state import State
 from .upload_file import UploadFile
-from .url import URL, URLPath
+from .url import URL, URLPath, QueryParams
 
 __all__ = (
     'URL',
@@ -14,5 +14,6 @@ __all__ = (
     'UploadFile',
     'State',
     'URLPath',
-    'Cookie'
+    'Cookie',
+    'QueryParams'
 )
