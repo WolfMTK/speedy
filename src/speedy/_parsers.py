46a11254874e1c582fd7aa7fea9e6a3dad7f7dcf
Alexander <qrer71@yandex.ru>
2024-08-31 23:54:33 +0300

    Update signature and add function to parse cookie

--- src/speedy/_parsers.py
+++ src/speedy/_parsers.py
@@ -1,8 +1,25 @@
 from functools import lru_cache
-from urllib.parse import parse_qsl
+from http.cookies import _unquote as unquote_cookie
+from urllib.parse import parse_qsl, unquote
 
 
 @lru_cache(1024)
-def parse_query_string(query: bytes) -> tuple[tuple[str, str], ...]:
+def parse_query_string(query: bytes | str) -> tuple[tuple[str, str], ...]:
     """ Parse a query string info a tuple of key value parse. """
-    return tuple(parse_qsl(query.decode('latin-1'), keep_blank_values=True, separator='&'))
+    if isinstance(query, bytes):
+        query = query.decode('latin-1')
+    return tuple(parse_qsl(query, keep_blank_values=True, separator='&'))
+
+
+@lru_cache(1024)
+def parse_cookie_string(cookie_string: str) -> dict[str, str]:
+    """ Parse a cookie string info a dict of key value parse. """
+    cookies = [cookie.split("=", 1) if "=" in cookie else ("", cookie) for cookie in cookie_string.split(";")]
+    output: dict[str, str] = {
+        k: unquote(unquote_cookie(v))
+        for k, v in filter(
+            lambda x: x[0] or x[1],
+            ((k.strip(), v.strip()) for k, v in cookies),
+        )
+    }
+    return output
