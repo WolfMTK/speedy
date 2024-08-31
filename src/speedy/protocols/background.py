1d2781b333edbf2f5924d8bd9a6c81d9224f96d7
Alexander <qrer71@yandex.ru>
2024-08-31 15:53:56 +0300

    Delete module

--- src/speedy/protocols/background.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class AbstractBackground(ABC):
-    """ Abstract a container for a 'background' task function. """
-
-    @abstractmethod
-    async def __call__(self) -> None: ...
