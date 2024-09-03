436e94efb2836fe65905c5faa155b6cf19f0eec0
Alexander <qrer71@yandex.ru>
2024-09-03 18:18:50 +0300

    Update AbstractRequest

--- src/speedy/protocols/request.py
+++ src/speedy/protocols/request.py
@@ -1,16 +1,31 @@
-from abc import ABC, abstractmethod
-from typing import AsyncGenerator
+from abc import abstractmethod
+from typing import AsyncGenerator, Protocol, Any
 
+from speedy.datastructures import FormMultiDict
+from speedy.protocols.connection import Connection, UserT, AuthT, StateT
 from speedy.types import Method
 
 
-class AbstractRequest(ABC):
+class AbstractRequest(Protocol[UserT, AuthT, StateT], Connection[UserT, AuthT, StateT]):
     @property
     @abstractmethod
     def method(self) -> Method: ...
+
+    @property
+    @abstractmethod
+    def content_type(self) -> tuple[str, dict[str, str]]: ...
 
     @abstractmethod
     async def stream(self) -> AsyncGenerator[bytes, None]: ...
 
     @abstractmethod
     async def body(self) -> bytes: ...
+
+    @abstractmethod
+    async def json(self) -> Any: ...
+
+    @abstractmethod
+    async def form(self, multipart_limit: int = 1000) -> FormMultiDict: ...
+
+    @abstractmethod
+    async def send_push_promise(self, path: str) -> None: ...
