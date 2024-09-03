651ef469eef5ba223c26176ac630047b61f80adb
Alexander <qrer71@yandex.ru>
2024-09-03 12:58:19 +0300

    Add function to parse an url encoded form data dict

--- src/speedy/_parsers.py
+++ src/speedy/_parsers.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from functools import lru_cache
 from http.cookies import _unquote as unquote_cookie
 from urllib.parse import parse_qsl, unquote
@@ -15,9 +16,9 @@ def parse_query_string(query: bytes | str) -> tuple[tuple[str, str], ...]:
 def parse_cookie_string(cookie_string: str) -> dict[str, str]:
     """ Parse a cookie string info a dict of key value parse. """
     cookies = [
-        cookie.split("=", 1)
-        if "=" in cookie
-        else ("", cookie)
+        cookie.split('=', 1)
+        if '=' in cookie
+        else ('', cookie)
         for cookie in cookie_string.split(";")
     ]
     output: dict[str, str] = {
@@ -30,3 +31,16 @@ def parse_cookie_string(cookie_string: str) -> dict[str, str]:
         )
     }
     return output
+
+
+@lru_cache(1024)
+def parse_url_encoded_form_data(encoded_data: bytes) -> dict[str, str | list[str]]:
+    """ Parse an url encoded form data dict. """
+    decoded = defaultdict(list)
+    for key, value in parse_qsl(encoded_data.decode('latin-1'), separator='&', keep_blank_values=True):
+        decoded[key].append(value)
+    return {
+        key: value
+        if len(value) > 1 else value[0]
+        for key, value in decoded.items()
+    }
