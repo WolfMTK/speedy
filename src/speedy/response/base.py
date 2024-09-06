59f972b1135f000033e73ae4e8599d06ac81e841
Alexander <qrer71@yandex.ru>
2024-09-06 14:02:08 +0300

    Update class Response
    Delete module

--- src/speedy/response/base.py
+++ src/speedy/response/base.py
@@ -1,89 +1,63 @@
+from collections.abc import Mapping, Sequence
 from datetime import datetime
-from typing import Any, Mapping, overload, Sequence, Generator
+from typing import Generic, TypeVar, Any
 
-from speedy import BackgroundTask, MediaType, ScopeType
-from speedy.datastructures import MutableHeaders
-from speedy.datastructures.cookie import Cookie
-from speedy.protocols.response import AbstractResponse
-from speedy.status_code import HTTP_200_OK, HTTP_204_NO_CONTENT
-from speedy.types import SAMESITE, Scope, ASGIReceiveCallable, ASGISendCallable
+from speedy import BackgroundTask, BackgroundTasks, MediaType
+from speedy.datastructures import Headers, MutableHeaders, Cookie
+from speedy.status_code import HTTP_200_OK, HTTP_204_NO_CONTENT, HTTP_304_NOT_MODIFIED
+from speedy.types import SAMESITE, ASGISendCallable, ASGIReceiveCallable, Scope
 
+T = TypeVar('T')
 ZERO_VALUE = 0
 
 
-class Response(AbstractResponse):
-    """ Base response class. """
-
-    _raw_header: list[tuple[bytes, bytes]]
-
+class Response(Generic[T]):
     def __init__(
             self,
-            content: Any | None = None,
-            status_code: int = HTTP_200_OK,
-            encoding: str = 'utf-8',
+            content: T | None = None,
+            background: BackgroundTask | BackgroundTasks | None = None,
+            headers: Headers | Mapping[str, str] | None = None,
             cookie: Sequence[Cookie] | Mapping[str, str] | None = None,
-            headers: Mapping[str, str] | None = None,
             media_type: MediaType | str | None = None,
-            background: BackgroundTask | None = None
+            status_code: int | None = HTTP_200_OK,
+            encoding: str = 'utf-8'
     ) -> None:
         self.background = background
+        self.headers: MutableHeaders = self._init_headers(headers)
         self.media_type = media_type
+        self.status_code = status_code
         self.encoding = encoding
         self.cookies = self._init_cookie(cookie)
-        self.status_code = status_code
         self.body = self.render(content)
-        self.init_headers(headers)
 
-    async def __call__(self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None:
-        prefix = 'websocket.' if scope['type'] == ScopeType.WEBSOCKET else ''
+    async def __call__(self, scope: Scope, recieve: ASGIReceiveCallable, send: ASGISendCallable) -> None:
+        prefix = 'websocket.' if scope['type'] == 'websocket' else ''
+        await send(
+            {
+                'type': prefix + 'http.response.start',
+                'status': self.status_code,
+                'headers': self.headers.raw,
+            }
+        )
+        await send({
+            'type': prefix + 'http.response.body',
+            'body': self.body,
+        })
 
-        await self.start_response(prefix, send)
-        await self.send_body(prefix, send, receive)
         if self.background is not None:
             await self.background()
-
-    @property
-    def raw_header(self) -> list[tuple[bytes, bytes]]:
-        """ Get the raw headers. """
-        return self._raw_header + [*self._encode_cookies()]
-
-    @property
-    def headers(self) -> MutableHeaders:
-        """ Get the headers. """
-        if not hasattr(self, '_headers'):
-            self._headers = MutableHeaders(self.raw_header)
-        return self._headers
-
-    @overload
-    def set_cookie(self, /, cookie: Cookie) -> None:
-        ...
-
-    @overload
-    def set_cookie(
-            self,
-            key: str,
-            value: str | None = None,
-            max_age: int | None = None,
-            expires: datetime | str | int | None = None,
-            path: str = '/',
-            domain: str | None = None,
-            secure: bool = False,
-            httponly: bool = False,
-            samesite: SAMESITE = 'lax'
-    ) -> None:
-        ...
 
     def set_cookie(
             self,
             key: str | Cookie,
-            value: str | None = None,
+            value: str = '',
             max_age: int | None = None,
             expires: datetime | str | int | None = None,
             path: str = '/',
             domain: str | None = None,
-            secure: bool = False,
-            httponly: bool = False,
-            samesite: SAMESITE = 'lax'
+            secure: bool | None = False,
+            httponly: bool | None = False,
+            samesite: SAMESITE = 'lax',
     ) -> None:
         """ Set a cookie on the response. """
         if not isinstance(key, Cookie):
@@ -123,86 +97,49 @@ class Response(AbstractResponse):
         self.cookies = [val for val in self.cookies if val != cookie]
         self.cookies.append(cookie)
 
-    async def start_response(self, prefix: str, send: ASGISendCallable) -> None:
-        """ Emit the start event of the response. """
-        await send(
-            {
-                'type': prefix + 'http.response.start',
-                'status': self.status_code,
-                'headers': self.raw_header
-            }
-        )
-
-    async def send_body(self, prefix: str, send: ASGISendCallable, receive: ASGIReceiveCallable) -> None:
-        """ Emit the response body. """
-        await send(
-            {
-                'type': prefix + 'http.response.body',
-                'body': self.body
-            }
-        )
-
-    def render(self, content: Any | None) -> bytes:
+    def render(self, content: Any) -> bytes:
         """ Handle the rendering of content into a bytes string. """
         if content is None:
             return b''
         if isinstance(content, bytes):
             return content
-        return content.encode(self.encoding)  # type: ignore
+        return content.encode(self.encoding)
 
-    def init_headers(self, headers: Mapping[str, str] | None = None) -> None:
-        """ Initializing headers and adding them to the common scope. """
-        raw_headers = self._to_raw_headers(headers)
-        self._set_content_length(raw_headers)
-        self._set_content_type(raw_headers)
-        self._raw_header = raw_headers
+    def set_header(self, key: str, value: Any) -> None:
+        """ Set a header on the response. """
+        self.headers[key] = value
 
-    def _set_content_length(self, raw_headers: list[tuple[bytes, bytes]]) -> None:
-        is_content_length = self._is_content_length(raw_headers)
-        is_status = self.status_code < HTTP_200_OK or self.status_code in (HTTP_204_NO_CONTENT, HTTP_204_NO_CONTENT)
-        body = getattr(self, 'body', None)
+    def _init_headers(self, headers: Headers | Mapping[str, str] | None) -> MutableHeaders:
+        raw_headers = {}
 
-        if body is not None and is_content_length and not is_status:
-            content_length = str(len(body))
-            raw_headers.append((b'content_length', content_length.encode('latin-1')))
+        if isinstance(headers, Headers):
+            raw_headers = {key: value for key, value in headers.items()}
+        elif isinstance(headers, Mapping):
+            raw_headers = dict(headers)
 
-    def _set_content_type(self, raw_headers: list[tuple[bytes, bytes]]) -> None:
-        is_content_type = self._is_content_type(raw_headers)
-        content_type = self.media_type
+        keys = [key.lower() for key in raw_headers.keys()]
+        is_content_length = 'content-length' in keys
+        is_content_type = 'content-type' in keys
+
+        if self.body is not None and is_content_length and not (
+                self.status_code < HTTP_200_OK or self.status_code in (HTTP_204_NO_CONTENT, HTTP_304_NOT_MODIFIED)
+        ):
+            content_length = str(len(self.body))
+            raw_headers['Content-Length'] = content_length
+
+        content_type = self._get_content_type()
         if content_type is not None and is_content_type:
             if content_type.startswith('text/') and 'charset=' not in content_type.lower():
-                content_type += '; charset=' + self.encoding
-            raw_headers.append((b'content-type', content_type.encode('latin-1')))
+                content_type += f'; charset={self.encoding}'
+            raw_headers['Content-Type'] = content_type
+        return MutableHeaders(raw_headers)
 
-    def _is_content_length(self, raw_headers: list[tuple[bytes, bytes]]) -> bool:
-        if len(raw_headers) == ZERO_VALUE:
-            return True
-        keys = [header[0] for header in raw_headers]
-        return b'content-length' not in keys
-
-    def _is_content_type(self, raw_headers: list[tuple[bytes, bytes]]) -> bool:
-        if len(raw_headers) == ZERO_VALUE:
-            return True
-        keys = [header[0] for header in raw_headers]
-        return b'content-type' not in keys
-
-    def _to_raw_headers(self, headers: Mapping[str, str] | None = None) -> list[tuple[bytes, bytes]]:
-        if headers is None:
-            return []
-        return [
-            (key.lower().encode('latin-1'), value.encode('latin-1'))
-            for key, value in headers.items()
-        ]
+    def _get_content_type(self) -> str | None:
+        if isinstance(self.media_type, MediaType):
+            return self.media_type.name
+        return self.media_type
 
     def _init_cookie(self, cookie: Sequence[Cookie] | Mapping[str, str]) -> list[Cookie]:
         if isinstance(cookie, Mapping):
             return [Cookie(key=key, value=value) for key, value in cookie.items()]
         return list(cookie or [])
-
-    def _encode_cookies(self) -> Generator[tuple[bytes, bytes], Any, None]:
-        if len(self.cookies) == ZERO_VALUE:
-            yield ()
-            return
-
-        for cookie in self.cookies:
-            yield cookie.to_encoded_header()
