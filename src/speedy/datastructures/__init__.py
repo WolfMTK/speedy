47d73841a18b0c4523f8ad2b4b3f28fe65106277
wolf <qrer71@yandex.ru>
2025-08-15 17:57:23 +0300

    Update imports

--- src/speedy/datastructures/__init__.py
+++ src/speedy/datastructures/__init__.py
@@ -1,22 +1,29 @@
 from .cookie import Cookie
-from .headers import Headers, MutableHeaders
+from .headers import Headers, MutableHeaders, ETag, Header
 from .multi_dicts import ImmutableMultiDict, MultiDict, FormMultiDict
+from .response_header import ResponseHeader
+from .secrets_values import SecretBytes, SecretString
 from .state import State, ImmutableState
 from .upload_file import UploadFile
 from .url import URL, URLPath, QueryParams, Address
 
 __all__ = (
-    'URL',
-    'ImmutableMultiDict',
-    'MultiDict',
-    'Headers',
-    'MutableHeaders',
-    'UploadFile',
-    'State',
-    'ImmutableState',
-    'URLPath',
-    'Cookie',
-    'QueryParams',
-    'Address',
-    'FormMultiDict'
+    "URL",
+    "ImmutableMultiDict",
+    "MultiDict",
+    "Headers",
+    "MutableHeaders",
+    "UploadFile",
+    "State",
+    "ImmutableState",
+    "URLPath",
+    "Cookie",
+    "QueryParams",
+    "Address",
+    "FormMultiDict",
+    "ETag",
+    "Header",
+    "SecretBytes",
+    "SecretString",
+    "ResponseHeader",
 )
