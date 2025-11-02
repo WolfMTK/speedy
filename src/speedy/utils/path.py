29b5d7927ad0654a6d17e1d3f63d1a6e7187d558
Wolf <qrer71@yandex.ru>
2025-11-02 10:27:34 +0300

    Add function for normalizes path

---
+++ src/speedy/utils/path.py
@@ -0,0 +1,8 @@
+import re
+
+
+def normalize_path(path: str) -> str:
+    " Normalizes the given path string. "
+    path = path.strip("/")
+    path = f"/{path}"
+    return re.compile("//+").sub("/", path)
