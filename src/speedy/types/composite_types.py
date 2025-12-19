addc6a3ca53bf74519a4daf8856eaee43fe57200
Alexander (wolf) <qrer71@yandex.ru>
2025-12-19 15:01:43 +0300

    Add new type `Scopes`

--- src/speedy/types/composite_types.py
+++ src/speedy/types/composite_types.py
@@ -1,9 +1,9 @@
 from collections.abc import Mapping, Callable
+from typing import Literal
 from typing import TypeAlias, Sequence, TYPE_CHECKING, Any, MutableMapping, Union
 
-
-
 if TYPE_CHECKING:
+    from speedy import ScopeType
     from speedy.datastructures import Cookie, ResponseHeader
     from speedy.params import ParameterKwarg
     from speedy.types.asgi_types import ASGIApplication
@@ -22,3 +22,5 @@ Middleware: TypeAlias = Callable[..., "ASGIApplication"]
 ParametersMap: TypeAlias = "Mapping[str, ParameterKwarg]"
 
 ExceptionHandlersMap: TypeAlias = "MutableMapping[Union[int, type[Exception]], ExceptionHandler]"
+
+Scopes: TypeAlias = "set[Literal[ScopeType.HTTP, ScopeType.WEBSOCKET]]"
