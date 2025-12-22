b32f04c3e45a5a0ecf3e72441fc5dc42bb360c80
Alexander (wolf) <qrer71@yandex.ru>
2025-12-22 13:24:05 +0300

    Add `AsyncWebSocketTestSession`

--- src/speedy/testing/websocket_test_session.py
+++ src/speedy/testing/websocket_test_session.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 import contextlib
+import math
 from typing import Any, TYPE_CHECKING
 
 import anyio.abc
+from anyio.streams.stapled import StapledObjectStream
 
-from speedy.types import WebSocketScope
+from speedy.types import WebSocketScope, ASGIAppType, WebSocketSendMessage, WebSocketReceiveMessage
 
 if TYPE_CHECKING:
     from speedy.testing.client.sync_client import TestClient
@@ -23,4 +25,29 @@ class WebSocketTestSession:
         self._portal = portal
         self._Client = client
         self._scope = scope
+        self._connect_timeout = connect_timeout
+
+
+class AsyncWebSocketTestSession:
+    def __init__(
+            self,
+            *,
+            app: ASGIAppType,
+            scope: WebSocketScope,
+            connect_timeout: float | None = None,
+            tg: anyio.abc.TaskGroup,
+    ) -> None:
+        self.scope = scope
+        self.accepted_subprotocol: str | None = None
+        self.extra_headers: list[tuple[bytes, bytes]] = []
+        self.app = app
+
+        self._tg = tg
+        self._send_stream = StapledObjectStream(
+            *anyio.create_memory_object_stream["WebSocketSendMessage"](math.inf)
+        )
+        self._receive_stream = StapledObjectStream(
+            *anyio.create_memory_object_stream["WebSocketReceiveMessage"](math.inf)
+        )
+        self._exit_stack = contextlib.AsyncExitStack()
         self._connect_timeout = connect_timeout
