f383d4b96f3abd39bb12ceae167e564acfa1a38c
Alexander (wolf) <qrer71@yandex.ru>
2025-12-23 14:33:46 +0300

    Add support `trio`

--- src/speedy/concurrency.py
+++ src/speedy/concurrency.py
@@ -1,11 +1,16 @@
+from __future__ import annotations
+
 import asyncio
 import contextvars
 import functools
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
-from typing import TypeVar, ParamSpec, Callable
+from typing import TypeVar, ParamSpec, Callable, TYPE_CHECKING
 
-import sniffio
+from speedy.utils._impl import current_async_library, AsyncLibraryNotFoundError
+
+if TYPE_CHECKING:
+    import trio
 
 T = TypeVar('T')
 P = ParamSpec('P')
@@ -31,24 +36,63 @@ async def _run_sync_asyncio(
     )
 
 
+async def _run_sync_trio(
+        obj: Callable[P, T],
+        *args: P.args,
+        **kwargs: P.kwargs
+) -> T:
+    import trio
+
+    bound_function = functools.partial(obj, *args, **kwargs)
+    return await trio.to_thread.run_sync(
+        bound_function,
+        limiter=get_trio_capacity_limiter(),
+    )
+
+
 async def sync_to_thread(
         obj: Callable[P, T],
         *args: P.args,
         **kwargs: P.kwargs
 ) -> T:
-    """
-    Run a synchronous call to an object in an asynchronous thread.
-    """
-    match sniffio.current_async_library():
+    """ Run a synchronous call to an object in an asynchronous thread. """
+    match current_async_library():
         case 'asyncio':
             return await _run_sync_asyncio(obj, *args, **kwargs)
+        case "trio":
+            return await _run_sync_trio(obj, *args, **kwargs)
         case _:
             raise RuntimeError('Unsupported async library')
 
 
+def set_asyncio_executor(executor: ThreadPoolExecutor | None) -> None:
+    """ Set the executor in which synchronous callables will be run within an asyncio context. """
+    try:
+        current_async_library()
+    except AsyncLibraryNotFoundError:
+        pass
+    else:
+        raise RuntimeError("Cannot set executor fron running loop")
+
+    _State.EXECUTOR = executor
+
+
 def get_asyncio_executor() -> ThreadPoolExecutor | None:
-    """
-    Getting the executor in which to execute synchronous calls
-    in asyncio context.
-    """
+    """ Getting the executor in which to execute synchronous calls in asyncio context. """
     return _State.EXECUTOR
+
+
+def get_trio_capacity_limiter() -> trio.CapacityLimiter | None:
+    """ Get the capacity limiter user when running synchronous callable within a trio context. """
+    return _State.LIMITER
+
+
+def set_trio_capacity_limiter(limiter: trio.CapacityLimiter | None) -> None:
+    try:
+        current_async_library()
+    except AsyncLibraryNotFoundError:
+        pass
+    else:
+        raise RuntimeError("Cannot set limiter while in async context")
+
+    _State.LIMITER = limiter
