b02ef4b804db442679a70292025798d1a3c77d3c
Alexander (wolf) <qrer71@yandex.ru>
2025-12-23 14:34:04 +0300

    Add tests to check support `trio`

--- tests/test_cuncurrency.py
+++ tests/test_cuncurrency.py
@@ -1,12 +1,44 @@
 import asyncio
+from concurrent.futures.thread import ThreadPoolExecutor
+from typing import Iterator
 
-from speedy.concurrency import sync_to_thread
+import pytest
+import trio
+
+from speedy.concurrency import sync_to_thread, get_asyncio_executor, set_asyncio_executor, get_trio_capacity_limiter, \
+    set_trio_capacity_limiter
 
 
-def func():
+def func() -> int:
     return 1
 
 
-def test_sync_to_thread():
+@pytest.fixture
+def loop() -> Iterator[asyncio.AbstractEventLoop]:
     loop = asyncio.new_event_loop()
+    try:
+        yield loop
+    finally:
+        loop.close()
+
+
+def test_sync_to_thread_asyncio(loop: asyncio.AbstractEventLoop) -> None:
     assert loop.run_until_complete(sync_to_thread(func)) == 1
+
+
+def test_sync_to_thread_trio() -> None:
+    assert trio.run(sync_to_thread, func) == 1
+
+
+def test_get_set_asyncio_executor() -> None:
+    assert get_asyncio_executor() is None
+    executor = ThreadPoolExecutor()
+    set_asyncio_executor(executor)
+    assert get_asyncio_executor() is executor
+
+
+def test_get_set_trio_capacity_limiter() -> None:
+    limiter = trio.CapacityLimiter(10)
+    assert get_trio_capacity_limiter() is None
+    set_trio_capacity_limiter(limiter)
+    assert get_trio_capacity_limiter() is limiter
