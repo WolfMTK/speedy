b2598038eb399529b145573f5f9541d7df59705b
Alexander (wolf) <qrer71@yandex.ru>
2025-12-25 15:52:57 +0300

    Add tests to check `session_data`

--- tests/testing/test_test_client.py
+++ tests/testing/test_test_client.py
@@ -1,14 +1,23 @@
 import pytest
 
-from speedy.testing import create_test_client
+from speedy.testing import create_test_client, create_async_test_client
 from speedy.types import AnyIOBackend
 
 
 @pytest.mark.parametrize("anyio_backend", ["asyncio", "trio"])
-def test_test_client_get_session_data_no_backend(anyio_backend: "AnyIOBackend"):
+def test_test_client_get_set_session_data_no_backend(anyio_backend: "AnyIOBackend"):
     with create_test_client(backend=anyio_backend) as client:
         with pytest.raises(RuntimeError, match="Session backend not configured") as err:
             client.set_session_data({})
 
         with pytest.raises(RuntimeError, match="Session backend not configured"):
             client.get_session_data()
+
+
+async def test_test_client_get_set_session_data_no_backend_async() -> None:
+    async with create_async_test_client() as client:
+        with pytest.raises(RuntimeError, match="Session backend not configured") as err:
+           await client.set_session_data({})
+
+        with pytest.raises(RuntimeError, match="Session backend not configured"):
+            await client.get_session_data()
