4d389c97a3225c2ccfcb5dd19d9cf4b29d02613d
Alexander (wolf) <qrer71@yandex.ru>
2025-12-25 15:52:40 +0300

    Add function `create_async_test_client`

--- src/speedy/testing/__init__.py
+++ src/speedy/testing/__init__.py
@@ -1,5 +1,6 @@
-from .helpers import create_test_client
+from .helpers import create_test_client, create_async_test_client
 
 __all__ = (
     "create_test_client",
+    "create_async_test_client",
 )
