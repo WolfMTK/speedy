88e3f01e8d1eaf6538711b367e0bf9f8a04e7c83
Alexander (wolf) <qrer71@yandex.ru>
2025-12-26 15:08:09 +0300

    Add `ExceptionHandlerMiddleware`

---
+++ src/speedy/middleware/_internal/exceptions/middleware.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+from speedy.connection import Request
+from speedy.exceptions.responses import create_exception_response
+from speedy.response import Response
+from speedy.types import ASGIAppType, Scope, Receive, Send, Message
+from speedy.utils.scope.state import ScopeState
+
+
+class ExceptionHandlerMiddleware:
+    """ Middleware used to wrap an ASGIAppType inside a try catch block and handle any exceptions raised. """
+
+    def __init__(self, app: ASGIAppType) -> None:
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        scope_state = ScopeState.from_scope(scope)
+
+        async def capture_response_started(event: Message) -> None:
+            if event["type"] == "http.response.start":
+                scope_state.response_started = True
+            await send(event)
+
+        await self.app(scope, receive, capture_response_started)
+
+    def default_http_exception_handler(self, request: Request, exc: Exception) -> Response[Any]:
+        """ Handle an HTTP exception by returning the appropriate response. """
+        return create_exception_response(request=request, exc=exc)
