16721aaa73c0591c2a347ecf1fb7d3e800e78967
Alexander (wolf) <qrer71@yandex.ru>
2025-12-26 13:15:15 +0300

    Add exception handling

--- src/speedy/_asgi/asgi_router.py
+++ src/speedy/_asgi/asgi_router.py
@@ -1,8 +1,16 @@
 from __future__ import annotations
 
+from traceback import format_exc
 from typing import TYPE_CHECKING
 
-from speedy.types import LifeSpanReceive, LifeSpanSend, LifespanShutdownCompleteEvent, LifespanStartupCompleteEvent
+from speedy.types import (
+    LifeSpanReceive,
+    LifeSpanSend,
+    LifespanShutdownCompleteEvent,
+    LifespanStartupCompleteEvent,
+    LifespanStartupFailedEvent,
+    LifespanShutdownFailedEvent,
+)
 
 if TYPE_CHECKING:
     from speedy import Speedy
@@ -20,8 +28,27 @@ class ASGIRouter:
         shutdown_event: LifespanShutdownCompleteEvent = {"type": "lifespan.shutdown.complete"}
 
         await receive()
-        async with self.app.lifespan():
-            await send(startup_event)
-            await receive()
+        started = False
+        try:
+            async with self.app.lifespan():
+                await send(startup_event)
+                started = True
+                await receive()
+        except BaseException as e:
+            formatted_exception = format_exc()
+
+            if started:
+                fail_message: LifespanShutdownFailedEvent = {
+                    "type": "lifespan.shutdown.failed",
+                    "message": formatted_exception,
+                }
+            else:
+                fail_message: LifespanStartupFailedEvent = {
+                    "type": "lifespan.startup.failed",
+                    "message": formatted_exception,
+                }
+
+            await send(fail_message)
+            raise e
 
         await send(shutdown_event)
