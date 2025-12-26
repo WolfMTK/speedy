86f526ad5d1d36957d5d461e904396da8831611f
Alexander (wolf) <qrer71@yandex.ru>
2025-12-26 13:15:28 +0300

    Add tests to check `lifespan`

--- tests/_asgi/test_asgi_router.py
+++ tests/_asgi/test_asgi_router.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 
 from contextlib import asynccontextmanager
 from typing import Callable, AsyncGenerator
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, MagicMock, call
 
+import anyio
 import pytest
+from pytest_mock import MockerFixture
 
 from speedy import Speedy
+from speedy._asgi import ASGIRouter
 from speedy.testing import create_test_client
 from speedy.testing.client import TestClient
 
@@ -104,3 +107,80 @@ def test_multiple_lifespan_managers() -> None:
 
     assert all(val.call_count == 1 for val in startup_mocks)
     assert all(val.call_count == 1 for val in shutdown_mocks)
+
+
+@pytest.fixture
+def mock_format_exc(mocker: MockerFixture) -> MagicMock:
+    return mocker.patch("speedy._asgi.asgi_router.format_exc")
+
+
+async def test_lifespan_startup_fail(mock_format_exc: MagicMock) -> None:
+    receive = AsyncMock()
+    receive.return_value = {"type": "lifespan.startup"}
+    send = AsyncMock()
+    exception = ValueError("foo")
+    mock_format_exc.return_value = str(exception)
+
+    mock_on_startup = AsyncMock(side_effect=exception)
+
+    async def on_startup() -> None:
+        await mock_on_startup()
+
+    router = ASGIRouter(app=Speedy(on_startup=[on_startup]))
+
+    with pytest.raises(ValueError, match="foo"):
+        await router.lifespan(receive, send)
+
+    assert send.call_count == 1
+    send.assert_called_once_with({"type": "lifespan.startup.failed", "message": mock_format_exc.return_value})
+
+
+async def test_lifespan_shutdown_fail(mock_format_exc: MagicMock) -> None:
+    receive = AsyncMock()
+    receive.return_value = {"type": "lifespan.shutdown"}
+    send = AsyncMock()
+    exception = ValueError("foo")
+    mock_format_exc.return_value = str(exception)
+
+    mock_on_shutdown = AsyncMock(side_effect=exception)
+
+    async def on_shutdown() -> None:
+        await mock_on_shutdown()
+
+    router = ASGIRouter(app=Speedy(on_shutdown=[on_shutdown]))
+
+    with pytest.raises(ValueError, match="foo"):
+        await router.lifespan(receive, send)
+
+    assert send.call_count == 2
+    assert send.call_args_list[1][0][0] == {"type": "lifespan.shutdown.failed", "message": mock_format_exc.return_value}
+
+
+async def test_lifespan_context_exception_after_startup(mock_format_exc: MagicMock) -> None:
+    receive = AsyncMock()
+    receive.return_value = {"type": "lifespan.startup"}
+    send = AsyncMock()
+    mock_format_exc.return_value = "foo"
+
+    async def sleep_and_raise() -> None:
+        await anyio.sleep(0)
+        raise ValueError("error")
+
+    @asynccontextmanager
+    async def lifespan(_: Speedy) -> AsyncGenerator[None, None]:
+        async with anyio.create_task_group() as tg:
+            tg.start_soon(sleep_and_raise)
+            yield
+
+    router = ASGIRouter(app=Speedy(lifespan=[lifespan]))
+
+    with pytest.RaisesGroup(pytest.RaisesExc(ValueError, match="error"), flatten_subgroups=True):
+        await router.lifespan(receive, send)
+
+    assert receive.call_count == 2
+    send.assert_has_calls(
+        [
+            call({"type": "lifespan.startup.complete"}),
+            call({"type": "lifespan.shutdown.failed", "message": mock_format_exc.return_value}),
+        ]
+    )
