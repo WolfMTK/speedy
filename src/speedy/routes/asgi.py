482ef241b8d95c040a24d053c65724cc0a804d77
Alexander (wolf) <qrer71@yandex.ru>
2026-01-03 15:56:57 +0300

    Create a `handle` method to initialize the connection

--- src/speedy/routes/asgi.py
+++ src/speedy/routes/asgi.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+from speedy.connection.base import ASGIConnection
+from speedy.handlers import ASGIRouteHandler
 from speedy.routes.base import BaseRoute
 from speedy.types import Scope, Receive, Send
 
@@ -9,8 +13,18 @@ class ASGIRoute(BaseRoute[Scope]):
             self,
             *,
             path: str,
+            route_handler: ASGIRouteHandler,
     ) -> None:
+        self.route_handler = route_handler
         super().__init__(path=path)
 
     async def handle(self, scope: Scope, receive: Receive, send: Send) -> None:
-        """ ASGI app of the route. """
+        """ ASGI app that authorizes the connection and then awaits the handler function. """
+        handler_scope = scope.copy()
+
+        connection = ASGIConnection["ASGIRouteHandler", Any, Any, Any](
+            scope=handler_scope,
+            receive=receive,
+            send=send,
+        )
+        await self.route_handler.handle(connection=connection)
