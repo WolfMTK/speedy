c21408ccc61910edd83d00403ff8ecfc20b45e89
Alexander (wolf) <qrer71@yandex.ru>
2026-01-07 20:52:37 +0300

    Update types

--- src/speedy/types/internal_types.py
+++ src/speedy/types/internal_types.py
@@ -3,8 +3,9 @@ from typing import TYPE_CHECKING, Callable, Any, TypeAlias, NamedTuple
 if TYPE_CHECKING:
     from speedy.handlers.http_handlers.base import HTTPRouteHandler
     from speedy.handlers.websocket_handlers.base import WebsocketRouteHandler
+    from speedy.handlers.asgi import ASGIRouteHandler
 
-RouteHandlerType: TypeAlias = "HTTPRouteHandler | WebsocketRouteHandler"
+RouteHandlerType: TypeAlias = "HTTPRouteHandler | WebsocketRouteHandler | ASGIRouteHandler"
 
 RouterHandler: TypeAlias = "RouteHandlerType | Callable[..., Any]"
 
