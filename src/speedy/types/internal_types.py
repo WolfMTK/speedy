538b3a968f00a8b5f66efeae458f0bd962726c78
Alexander (wolf) <qrer71@yandex.ru>
2026-01-09 13:03:15 +0300

    Add new types

--- src/speedy/types/internal_types.py
+++ src/speedy/types/internal_types.py
@@ -1,19 +1,29 @@
-from typing import TYPE_CHECKING, Callable, Any, TypeAlias, NamedTuple
+from typing import TYPE_CHECKING, Any, Callable, Literal, NamedTuple, TypeAlias
 
 if TYPE_CHECKING:
+    from speedy.handlers.asgi import ASGIRouteHandler
+    from speedy.handlers.base import BaseRouteHandler
     from speedy.handlers.http_handlers.base import HTTPRouteHandler
     from speedy.handlers.websocket_handlers.base import WebsocketRouteHandler
-    from speedy.handlers.asgi import ASGIRouteHandler
+    from speedy.types import Method
 
-RouteHandlerType: TypeAlias = "HTTPRouteHandler | WebsocketRouteHandler | ASGIRouteHandler"
+RouteHandlerType: TypeAlias = (
+    "HTTPRouteHandler | WebsocketRouteHandler | ASGIRouteHandler"
+)
 
+RouterHandler: TypeAlias = "RouteHandlerType | Callable[..., Any]"
 RouterHandler: TypeAlias = "RouteHandlerType | Callable[..., Any]"
 
 ControllerRouterHandler: TypeAlias = "RouteHandlerType | Callable[..., Any]"
 
+RouteHanderMapItem: TypeAlias = (
+    'dict[Method | Literal["websocket", "asgi"], BaseRouteHandler]'
+)
+
 
 class PathParameterDefinition(NamedTuple):
     """Path parameter tuple."""
+
     name: str
     full: str
     type: type
