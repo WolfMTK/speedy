259495e7f4cf5ef3d77801f4a87bc5a92c0d66aa
Alexander (wolf) <qrer71@yandex.ru>
2026-01-11 19:22:32 +0300

    Add new constants

--- src/speedy/constants.py
+++ src/speedy/constants.py
@@ -11,3 +11,10 @@ REQUEST_MAX_BODY_SIZE: Final = 10_000_000
 REDIRECT_STATUS_CODE: Final = {301, 302, 303, 307, 308}
 
 REDIRECT_ALLOWED_MEDIA_TYPES: Final = {MediaType.TEXT, MediaType.HTML, MediaType.JSON}
+
+DEFAULT_ALLOWED_CORS_HEADERS: Final = {
+    "Accept",
+    "Accept-Language",
+    "Content-Language",
+    "Content-Type",
+}
