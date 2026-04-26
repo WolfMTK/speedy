8c27e69010f837c4d4be43ab51c8117739d77655
Alexander (wolf) <qrer71@yandex.ru>
2026-04-26 20:50:58 +0300

    Update imports

--- src/speedy/__init__.py
+++ src/speedy/__init__.py
@@ -1,7 +1,7 @@
-from .router import Router
-from .app import Speedy
-from .background import BackgroundTask, BackgroundTasks
-from .enums import MediaType, HttpMethod, ScopeType, RequestEncodingType
+from speedy.router import Router
+from speedy.app import Speedy
+from speedy.background import BackgroundTask, BackgroundTasks
+from speedy.enums import MediaType, HttpMethod, ScopeType, RequestEncodingType
 
 __all__ = (
     "BackgroundTask",
