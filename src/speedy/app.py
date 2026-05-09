ff123bf45fcb97b59a7deb8a7fb09614af8a6190
Alexander (wolf) <qrer71@yandex.ru>
2026-05-09 20:27:38 +0300

    Add prepared http methods

---
+++ src/speedy/app.py
@@ -0,0 +1,36 @@
+class Speedy:
+    def __init__(self) -> None: ...
+
+    def __call__(self) -> None: ...
+
+    def get(self) -> None:
+        # TODO: add decoration HTTPHandler
+        ...
+
+    def put(self) -> None:
+        # TODO: add decoration HTTPHandler
+        ...
+
+    def post(self) -> None:
+        # TODO: add decoration HTTPHandler
+        ...
+
+    def delete(self) -> None:
+        # TODO: add decoration HTTPHandler
+        ...
+
+    def options(self) -> None:
+        # TODO: add decoration HTTPHandler
+        ...
+
+    def head(self) -> None:
+        # TODO: add decoration HTTPHandler
+        ...
+
+    def path(self) -> None:
+        # TODO: add decoration HTTPHandler
+        ...
+
+    def trace(self) -> None:
+        # TODO: add decoration HTTPHandler
+        ...
