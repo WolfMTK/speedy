c4e33c9b115c9b7fb77e13f9c5870525c8989c0c
Alexander (wolf) <qrer71@yandex.ru>
2026-05-18 21:02:30 +0300

    Add router and method `register`

--- src/speedy/routing.py
+++ src/speedy/routing.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from typing import Sequence
 
 from speedy.types import Receive, Send, BaseScope
 
@@ -17,3 +18,17 @@ class BaseRoute[ScopeT: BaseScope](ABC):
             "Route subclasses must implement handle "
             "which serves as the ASGI app entry point",
         )
+
+
+class Router:
+    def __init__(
+            self,
+            route_handlers: Sequence[BaseRoute],
+    ) -> None:
+        self.route_handlers = tuple(route_handlers)
+
+    def register(self, value: BaseRoute) -> None:
+        if value in self:
+            # TODO: change exception
+            raise ValueError("Cannot register a router on itself")
+        self.route_handlers = (*self.route_handlers, value)
