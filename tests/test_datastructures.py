80b74499c850262a191afbf7c3dcb5ab965254d9
Alexander (wolf) <qrer71@yandex.ru>
2026-09-06 16:53:47 +0300

    Add tests to check structure `ImmutableMultiDict`

--- tests/test_datastructures.py
+++ tests/test_datastructures.py
@@ -4,8 +4,19 @@ from copy import copy, deepcopy
 from typing import Any, Iterator, Mapping
 
 import pytest
-from speedy.datastructures import URL, Address, ImmutableState, State, URLPath, Headers, MutableHeaders
+from speedy.datastructures import (
+    URL,
+    Address,
+    ImmutableState,
+    State,
+    URLPath,
+    Headers,
+    MutableHeaders,
+    ImmutableMultiDict,
+)
 from speedy.exceptions import StateException
+
+ZERO = 0
 
 
 class _CustomMapping(Mapping):
@@ -720,3 +731,72 @@ class TestMutableHeaders:
         assert dict(headers) == {"a": "1", "b": "2"}
         assert list(headers.items()) == [("a", "1"), ("b", "2",)]
         assert list(headers.raw) == [(b"a", b"1"), (b"b", b"2")]
+
+
+class TestImmutableMultiDict:
+    def test_immutable_multidict_getList(self) -> None:
+        multidict = ImmutableMultiDict([('a', '123'), ('a', '456'), ('b', '789')], c='12')
+        assert isinstance(multidict.getList('a'), list)
+        assert multidict.getList('a') == ['123', '456']
+        assert multidict.getList('b') == ['789']
+        assert multidict.getList('c') == ['12']
+
+    def test_immutable_multidict_multi_items(self) -> None:
+        multidict = ImmutableMultiDict([('a', '123'), ('a', '456'), ('b', '789')], c='12')
+        items = multidict.multi_items()
+        assert isinstance(items, list)
+        assert len(items) > 1
+        assert sorted(items) == sorted([('a', '123'), ('a', '456'), ('b', '789'), ('c', '12')])
+
+    def test_immutable_multidict(self) -> None:
+        multidict = ImmutableMultiDict([('a', '123'), ('a', '456'), ('b', '789')], c='12')
+        keys = ('a', 'b', 'c')
+        values = ('456', '789', '12')
+        assert sorted(multidict.keys()) == sorted(keys)
+        assert sorted(multidict.values()) == sorted(values)
+
+        for key, value in multidict.items():
+            assert key in keys
+            assert value in values
+
+        for key in multidict:
+            assert key in keys
+
+        assert multidict['a'] == '456'
+        values = ['444', '565']
+        multidict['b'] = values
+        assert multidict['b'] == values[-1]
+        assert sorted(multidict.getList('b')) == sorted(values)
+        assert multidict.pop('b') == values[-1]
+        assert len(multidict.getList('b')) == ZERO
+
+        kwargs = {'d': ['544', '231']}
+        for key, value in kwargs.items():
+            multidict[key] = value
+            assert multidict.popitem() == (key, value[-1])
+
+        for key, value in kwargs.items():
+            multidict[key] = value
+            assert sorted(multidict.poplist(key)) == sorted(value)
+
+        value = ('d', '300')
+        multidict.update([value])
+        assert multidict['d'] == value[-1]
+
+        value = {'d': '450'}
+        multidict.update(value)
+        assert multidict['d'] == value['d']
+
+        value = ImmutableMultiDict([('d', '200')])
+        multidict.update(value)
+        assert multidict['d'] == value['d']
+
+        del multidict['c']
+        assert multidict.get('c') is None
+
+        multidict.append('c', '322')
+        assert multidict['c'] == '322'
+
+        multidict.clear()
+        assert len(multidict.multi_items()) == ZERO
+        assert len(multidict.items()) == ZERO
