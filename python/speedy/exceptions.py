36204c5ebcc497aafd60b4505aefcd3fcdbabb5d
Alexander (wolf) <qrer71@yandex.ru>
2026-09-07 20:25:25 +0300

    Add exception `ValidationException`

--- python/speedy/exceptions.py
+++ python/speedy/exceptions.py
@@ -4,3 +4,7 @@ class ApplicationException(Exception):
 
 class StateException(ApplicationException):
     """State exception."""
+
+
+class ValidationException(ApplicationException):
+    """Validation exception."""
