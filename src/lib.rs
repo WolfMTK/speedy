ff98afd2e905ae619dc256ca5e5aee6dae90f6cb
Alexander (wolf) <qrer71@yandex.ru>
2026-09-08 21:50:46 +0300

    Add structure `Request` and exception `JSONDecodeError`

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,8 @@ mod _speedy {
         URLPath, UploadFile,
     };
     #[pymodule_export]
+    use crate::requests::{HTTPConnection, Request, parse_json, parse_urlencoded_form};
+    #[pymodule_export]
     use crate::responses::Response;
 
     #[pymodule_init]
@@ -24,6 +26,8 @@ mod _speedy {
         let py = m.py();
         let mapping_abc = py.import("collections.abc")?.getattr("Mapping")?;
         mapping_abc.call_method1("register", (m.getattr("Headers")?,))?;
+        mapping_abc.call_method1("register", (m.getattr("HTTPConnection")?,))?;
+        m.add("JSONDecodeError", py.get_type::<crate::requests::JSONDecodeError>())?;
         Ok(())
     }
 }
