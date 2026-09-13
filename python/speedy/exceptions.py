11cadbcbbc71fa9c249564012b481673fe195eb6
Alexander (wolf) <qrer71@yandex.ru>
2026-09-13 17:11:55 +0300

    Add exceptions `WebSocketDisconnected` and `WebSocketDisconnect`

--- python/speedy/exceptions.py
+++ python/speedy/exceptions.py
@@ -1,3 +1,6 @@
+from speedy.status import WS_1000_NORMAL_CLOSURE
+
+
 class ApplicationException(Exception):
     """Base exception."""
 
@@ -12,3 +15,15 @@ class ValidationException(ApplicationException):
 
 class ClientDisconnect(ApplicationException):
     """Client disconnect."""
+
+
+class WebSocketDisconnected(RuntimeError):
+    """Raised when attempting to use a disconnected WebSocket."""
+
+
+class WebSocketDisconnect(ApplicationException):
+    """Raised when the client disconnects from a WebSocket."""
+
+    def __init__(self, code: int = WS_1000_NORMAL_CLOSURE, reason: str | None = None) -> None:
+        self.code = code
+        self.reason = reason or ""
